afd2cbb073f35568682493ad0972015a06d0292b
Ene-George <ene.2456@gmail.com>
2022-06-10 10:32:01 +0300

    edit10.06

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-   // write your solution here
+
+// function about(){
+//   let aboutMe=document.querySelector(".right")
+//   let article=document.createElement("article")
+//   article.style.display="flex"
+//   article.style.justifyContent="center"
+//   article.style.alignItems="center"
+//   let description=document.createElement("p")
+//   description.style.textAlign="center"
+//   description.innerHTML=`I am a junior frontend developer. In the past I was a chef in a restaurant in the north of Bucharest.
+
+//   I decided to do a professional reconversion in IT from personal and professional reasons and passion
+  
+//   I took Frontend developer courses at Wantsome. (is a company from Iasi).
+//   And now I'm enrolled in a React course also taught (tot) by wantsome.`
+//   aboutMe.appendChild(article)
+//   article.appendChild(description)
+
+// }  
+//   about()
+
    fetch("data.json")
    .then(response => response.json())
    .then(data =>{
        window.mockyData=data
        
        creatUser();
+       contactDetails()
    })
    
 
@@ -63,6 +84,21 @@
       profil.appendChild(linkedin)
     }
    }
+   function contactDetails(){
+    let contactDetails=document.querySelector(".contactDetails")
+     for(user of mockyData){
+       let title=document.createElement("h3")
+       title.innerHTML=`<i class="fa fa-align-justify"></i>`+"Details:"
+       let linceS=document.createElement("div")
+       linceS.innerHTML=`<i class="fa fa-drivers-license"></i>`+"Driver's license: "+user.driver
+       let language=document.createElement("div")
+       language.innerHTML=`<i class="fa fa-comment"></i>`+"English: "+user.English
+       
+       contactDetails.appendChild(title)
+       contactDetails.appendChild(linceS)
+       contactDetails.appendChild(language)
+     }
+   }
 
    function openTab(evt, name) {
     let i, tabcontent, tablinks;
@@ -89,7 +125,7 @@ window.onclick = function(event) {
   }
 }
 
-// Creati study
+// Create study
 fetch("dataStudy.json")
 .then(response => response.json())
 .then(data =>{
@@ -132,4 +168,51 @@ function creatStudy(){
 
 
     }
+}
+// Create experience
+fetch("dataExperience.json")
+.then(response => response.json())
+.then(data =>{
+    window.mockyData=data
+    
+    creatExperience();
+})
+function creatExperience(){
+  let expContainer=document.querySelector("#Experience")
+  for(user of mockyData){
+    let section=document.createElement("section")
+    section.style.borderBottom="1px solid black"
+    let titleCompany=document.createElement("h3")
+    titleCompany.textContent="Company: "+user.company
+    let linkCompany=document.createElement("a")
+    linkCompany.innerHTML=`<i class="fa fa-map-pin"></i>Link: `
+    linkCompany.href=user.link
+    linkCompany.style.textDecoration="none"
+    linkCompany.style.color="black"
+    linkCompany.style.display="flex"
+    linkCompany.style.flexDirection="row"
+    linkCompany.style.justifyContent="space-between"
+    let imgCompany=document.createElement("img")
+    imgCompany.src=user.img
+    imgCompany.setAttribute("width","100px")
+    
+    let yearexp=document.createElement("p")
+    yearexp.textContent="Experience: "+user.experience
+    let activity=document.createElement('p')
+    activity.textContent="Activity: "+user.activity
+    let ocupation=document.createElement("p")
+    ocupation.textContent="Ocupation: "+user.ocupation
+    let description=document.createElement("p")
+    description.textContent="Description Job: "+user.description
+
+    expContainer.appendChild(section)
+    section.appendChild(titleCompany)
+    section.appendChild(linkCompany)
+    linkCompany.appendChild(imgCompany)
+    section.appendChild(yearexp)
+    section.appendChild(activity)
+    section.appendChild(ocupation)
+    section.appendChild(description)
+  }
+
 }
